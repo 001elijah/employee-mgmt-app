1a0b6ef9da7d44617e395e5f2549f7241f3e9cc6
Illia Zolotukha <illia@luday.se>
2023-08-25 01:06:53 +0300

    Project init; ADD: tailwind and web compilation

---
+++ employee_mgmt_app/client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+const nativewind = require("nativewind/tailwind/css")
+module.exports = {
+  content: ["./components/**/*.js", "./screens/**/*.js", "./App.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [nativewind],
+}
+
